d90e71f9b030a6243e67ae1c2ac3ad7a3bdf7c31
Landon Redmond <name1234561.po@gmail.com>
2023-10-06 11:25:56 -0500

    Add a get endpoint.

---
+++ src/routes/v1/restaurants/info/[slug]/+server.ts
@@ -0,0 +1,18 @@
+import { json } from "@sveltejs/kit";
+import { getCollection } from "$lib/database";
+import type { Collection } from "mongodb";
+import type { RequestEvent } from "@sveltejs/kit";
+
+const restaurantsCollection: Collection = getCollection("core", "restaurants");
+
+export async function GET({ params }: RequestEvent) {
+    const restaurant: string | undefined = params.slug;
+    const result = await restaurantsCollection.findOne({
+        name: restaurant,
+    });
+
+    return json({
+        success: true,
+        restaurant: result,
+    });
+}
