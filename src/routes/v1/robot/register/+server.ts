924d1b9a62771b935c3c38e05993e95529b9ddb0
Landon Redmond <name1234561.po@gmail.com>
2023-11-07 10:57:58 -0600

    Add return statement.

--- src/routes/v1/robot/register/+server.ts
+++ src/routes/v1/robot/register/+server.ts
@@ -1,10 +1,14 @@
-import type { RequestEvent } from "@sveltejs/kit";
+import { json, type RequestEvent } from "@sveltejs/kit";
 
 type registerEvent = {
-    url: string,
-}
+    url: string;
+};
 
 export async function POST({ request }: RequestEvent) {
     const data: registerEvent = await request.json();
     console.log("Registered robot.", data);
+
+    return json({
+        success: true,
+    });
 }
