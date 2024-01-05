df1605d6d57e2bb13f51a32d03570a6f5b78f73f
Landon Redmond <name1234561.po@gmail.com>
2024-01-05 10:47:39 -0600

    Change adapter to vercel.

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,12 @@
-import adapter from "@sveltejs/adapter-node";
-import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: [preprocess({
-        postcss: true,
-    })],
-
     kit: {
         adapter: adapter(),
-        csrf: {
-            checkOrigin: false,
-        },
     },
+    preprocess: vitePreprocess(),
 };
 
 export default config;
