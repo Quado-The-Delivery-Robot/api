be540c62d20e288c5e4d4339ac1b9142d01ec095
Landon Redmond <name1234561.po@gmail.com>
2024-01-10 10:55:53 -0600

    Give handle type.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,8 +3,9 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "@auth/core/providers/google";
 import { GOOGLE_CLIENT_ID, GOOGLE_SECERT } from "$env/static/private";
 import database from "$lib/database";
+import type { Handle } from "@sveltejs/kit";
 
-export const handle = SvelteKitAuth({
+export const handle: Handle = SvelteKitAuth({
     providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_SECERT, redirectProxyUrl: "https://example.com/auth" })],
     adapter: MongoDBAdapter(database, { databaseName: "app" }),
 });
