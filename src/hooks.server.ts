bcb2a61708095f99f252549ca6f319793bc410bf
Landon Redmond <name1234561.po@gmail.com>
2024-01-11 11:16:14 -0600

    Fix.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,7 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "@auth/core/providers/google";
-import { GOOGLE_CLIENT_ID, GOOGLE_SECERT, NODE_ENV } from "$env/static/private";
+import { GOOGLE_CLIENT_ID, GOOGLE_SECERT } from "$env/static/private";
 import database from "$lib/database";
 import type { Handle } from "@sveltejs/kit";
 
@@ -15,8 +15,7 @@ export const handle: Handle = SvelteKitAuth({
                 httpOnly: true,
                 sameSite: "lax",
                 path: "/",
-                secure: NODE_ENV === "production" ? true : false,
-                domain: "quadoapp.com",
+                domain: ".quadoapp.com",
             },
         },
     },
