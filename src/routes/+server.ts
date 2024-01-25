721bfb48c94d3a666776fdbd29c3e1bfd52378f6
Landon Redmond <name1234561.po@gmail.com>
2024-01-25 11:17:29 -0600

    Add session debug.

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,7 +1,12 @@
 import { json } from "@sveltejs/kit";
+import type { RequestEvent } from "@sveltejs/kit";
+import type { Session } from "@auth/sveltekit";
 
-export async function GET() {
+export async function GET({ locals }: RequestEvent) {
+    const session: Session = (await locals.getSession()) as Session;
+
     return json({
         status: "Functional.",
+        session: session,
     });
 }
