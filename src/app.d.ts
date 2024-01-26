b5a26ae05e56c93a66cb88ed5c58a4f5691ff2c8
Landon Redmond <name1234561.po@gmail.com>
2024-01-26 10:49:24 -0600

    Add session to locals.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,11 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
+import type { Session } from "@auth/sveltekit";
+
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+    namespace App {
+        interface Locals {
+            session: Session;
+        }
+    }
 }
 
 export {};
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,22 +1,37 @@
+import { sequence } from "@sveltejs/kit/hooks";
 import { SvelteKitAuth } from "@auth/sveltekit";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import GoogleProvider from "@auth/core/providers/google";
 import { GOOGLE_CLIENT_ID, GOOGLE_SECERT } from "$env/static/private";
 import database from "$lib/database";
 import type { Handle } from "@sveltejs/kit";
+import type { Session } from "@auth/sveltekit";
 
-export const handle: Handle = SvelteKitAuth({
-    providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_SECERT, redirectProxyUrl: "https://www.quadoapp.com/auth" })],
-    adapter: MongoDBAdapter(database, { databaseName: "app" }),
-    cookies: {
-        sessionToken: {
-            name: "authjs.session-token",
-            options: {
-                httpOnly: true,
-                sameSite: "lax",
-                path: "/",
-                domain: ".quadoapp.com",
+const addLocalsSession: Handle = async ({ event, resolve }) => {
+    const session: Session = (await event.locals.getSession()) as Session;
+    event.locals.session = session;
+
+    return resolve(event);
+};
+
+export const handle: Handle = sequence(
+    SvelteKitAuth({
+        providers: [GoogleProvider({ clientId: GOOGLE_CLIENT_ID, clientSecret: GOOGLE_SECERT, redirectProxyUrl: "https://www.quadoapp.com/auth" })],
+        adapter: MongoDBAdapter(database, { databaseName: "app" }),
+        pages: {
+            signIn: "/login",
+        },
+        cookies: {
+            sessionToken: {
+                name: "authjs.session-token",
+                options: {
+                    httpOnly: true,
+                    sameSite: "lax",
+                    path: "/",
+                    domain: ".quadoapp.com",
+                },
             },
         },
-    },
-});
+    }),
+    addLocalsSession
+);
