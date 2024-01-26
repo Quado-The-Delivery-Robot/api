58f3e1ad92e14ab0a1f574903b837da90a2f9c1b
Landon Redmond <name1234561.po@gmail.com>
2024-01-26 10:49:32 -0600

    Fix debug statement.

--- src/routes/v1/orders/get/+server.ts
+++ src/routes/v1/orders/get/+server.ts
@@ -8,14 +8,12 @@ import type { order } from "$lib/types";
 const ordersCollection: Collection = getCollection("core", "orders");
 
 export async function GET({ locals }: RequestEvent) {
-    const session: Session = (await locals.getSession()) as Session;
-
     return json({
-        session,
+        session: locals.session,
     });
 
     const result = ordersCollection.find({
-        user: session?.user?.email,
+        user: locals.session?.user?.email,
     });
     const orders: order[] = (await result.toArray()) as unknown as any;
 
