9cba7aec807dc1304d236ccade0940a67a444cf2
Landon Redmond <name1234561.po@gmail.com>
2024-01-31 11:28:32 -0600

    Remove unused type import.

--- src/routes/v1/orders/get/+server.ts
+++ src/routes/v1/orders/get/+server.ts
@@ -2,7 +2,6 @@ import { json } from "@sveltejs/kit";
 import { getCollection } from "$lib/database";
 import type { Collection } from "mongodb";
 import type { RequestEvent } from "@sveltejs/kit";
-import type { order } from "$lib/types";
 
 const ordersCollection: Collection = getCollection("core", "orders");
 
