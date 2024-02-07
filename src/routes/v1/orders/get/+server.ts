5a8838015ada4a05810e78027785ee9aeafd9b5e
Landon Redmond <name1234561.po@gmail.com>
2024-02-07 10:31:29 -0600

    Fetch actual restaurant name.

--- src/routes/v1/orders/get/+server.ts
+++ src/routes/v1/orders/get/+server.ts
@@ -2,19 +2,23 @@ import { json } from "@sveltejs/kit";
 import { getCollection } from "$lib/database";
 import type { Collection } from "mongodb";
 import type { RequestEvent } from "@sveltejs/kit";
-import type { order } from "$lib/types";
+import type { order, restaurant } from "$lib/types";
 
 const ordersCollection: Collection = getCollection("core", "orders");
 
-export async function GET({ locals }: RequestEvent) {
+export async function GET({ locals, fetch }: RequestEvent) {
     const result = await ordersCollection.findOne({
         id: locals.session.user?.email,
     });
     const items: order[] = result?.items || [];
 
-    // Remove the code from the orders.
-    items.forEach((order: order) => {
+    // Remove the code from the orders & replace the restaurant ID with the actual name.
+    items.forEach(async (order: order) => {
         order.code = null as unknown as any;
+
+        const restaurantFetch = await fetch(`/restaurants/info/${order.restaurant}`);
+        const restaurant: restaurant = await restaurantFetch.json();
+        order.restaurant = restaurant.name;
     });
 
     return json({
