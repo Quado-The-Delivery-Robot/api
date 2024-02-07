da1e4b480b76eff85c6018ea4839dbbe0a2b0ecb
Landon Redmond <name1234561.po@gmail.com>
2024-02-07 10:24:21 -0600

    Remove the code field from the orders.

--- src/routes/v1/orders/get/+server.ts
+++ src/routes/v1/orders/get/+server.ts
@@ -2,6 +2,7 @@ import { json } from "@sveltejs/kit";
 import { getCollection } from "$lib/database";
 import type { Collection } from "mongodb";
 import type { RequestEvent } from "@sveltejs/kit";
+import type { order } from "$lib/types";
 
 const ordersCollection: Collection = getCollection("core", "orders");
 
@@ -9,9 +10,15 @@ export async function GET({ locals }: RequestEvent) {
     const result = await ordersCollection.findOne({
         id: locals.session.user?.email,
     });
+    const items: order[] = result?.items || [];
+
+    // Remove the code from the orders.
+    items.forEach((order: order) => {
+        order.code = null as unknown as any;
+    });
 
     return json({
         success: true,
-        orders: result?.items,
+        orders: items,
     });
 }
