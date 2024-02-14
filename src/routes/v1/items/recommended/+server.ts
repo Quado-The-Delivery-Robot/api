183311ec4d9ccb5012cbc10dac2db1bf495190be
Landon Redmond <name1234561.po@gmail.com>
2024-02-14 10:56:14 -0600

    Fix endpoint.

--- src/routes/v1/items/recommended/+server.ts
+++ src/routes/v1/items/recommended/+server.ts
@@ -1,13 +1,9 @@
 import { json } from "@sveltejs/kit";
-import { getCollection } from "$lib/database";
-import type { Collection } from "mongodb";
 import type { restaurant, restaurantItem } from "$lib/types.js";
-
-const restaurantsCollection: Collection = getCollection("core", "restaurants");
 
 export async function GET({ fetch }) {
     const result = await fetch("/v1/restaurants/recommended");
-    const { restaurants }: { restaurants: restaurant[] } = await result.json();
+    const { data: restaurants }: { data: restaurant[] } = await result.json();
     const recommendedItems: restaurantItem[] = [];
 
     // Get the popular items from the recommended restaurants.
