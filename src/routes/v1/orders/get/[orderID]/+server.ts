a5c30dbe28dc5cbf9d0a320563159ee9766256e8
Landon Redmond <landon.redmond0@gmail.com>
2024-03-01 11:00:53 -0600

    Improve endpoint.

--- src/routes/v1/orders/get/[orderID]/+server.ts
+++ src/routes/v1/orders/get/[orderID]/+server.ts
@@ -1,7 +1,7 @@
 import { json } from "@sveltejs/kit";
 import { getCollection } from "$lib/database";
 import type { Collection } from "mongodb";
-import type { order } from "$lib/types";
+import type { order, restaurant } from "$lib/types";
 
 const ordersCollection: Collection = getCollection("core", "orders");
 
@@ -10,9 +10,18 @@ export async function GET({ locals, params }) {
         id: locals.session.user?.email,
     });
     const items: order[] = result?.items || [];
+    const order: order = items.filter((order: order) => order.id === params.orderID)[0];
+
+    if (order) {
+        order.code = null as unknown as any;
+
+        const restaurantFetch = await fetch(`/v1/restaurants/info/${order.restaurant}`);
+        const { restaurant }: { restaurant: restaurant } = await restaurantFetch.json();
+        order.restaurant = restaurant.name;
+    }
 
     return json({
         success: true,
-        order: items.filter((order: order) => order.id === params.orderID)[0],
+        order,
     });
 }
