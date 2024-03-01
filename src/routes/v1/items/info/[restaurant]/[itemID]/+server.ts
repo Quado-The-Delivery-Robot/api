bc29a15781eb20d11ea3eaaa1181e13bbb6bad14
Landon Redmond <landon.redmond0@gmail.com>
2024-03-01 10:20:00 -0600

    Add item info endpoint.

---
+++ src/routes/v1/items/info/[restaurant]/[itemID]/+server.ts
@@ -0,0 +1,15 @@
+import { json } from "@sveltejs/kit";
+import type { restaurant, restaurantItem } from "$lib/types.js";
+
+export async function GET({ fetch, params }) {
+    const itemID: number = parseInt(params.restaurant);
+    const result = await fetch(`/v1/restaurants/info/${params.restaurant}`, {
+        credentials: "include",
+    });
+    const { restaurant: restaurant }: { restaurant: restaurant } = await result.json();
+
+    return json({
+        success: restaurant.items[itemID] !== null,
+        item: restaurant.items[itemID],
+    });
+}
