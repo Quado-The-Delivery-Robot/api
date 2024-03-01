d9985ccd1e0a55aefe22657b536d73aef6a89517
Landon Redmond <landon.redmond0@gmail.com>
2024-03-01 10:53:37 -0600

    Fix order get endpoint.

--- src/routes/v1/orders/get/[orderID]/+server.ts
+++ src/routes/v1/orders/get/[orderID]/+server.ts
@@ -1,12 +1,11 @@
 import { json } from "@sveltejs/kit";
 import { getCollection } from "$lib/database";
 import type { Collection } from "mongodb";
-import type { RequestEvent } from "@sveltejs/kit";
 import type { order } from "$lib/types";
 
 const ordersCollection: Collection = getCollection("core", "orders");
 
-export async function GET({ locals, params }: RequestEvent) {
+export async function GET({ locals, params }) {
     const result = await ordersCollection.findOne({
         id: locals.session.user?.email,
     });
@@ -14,6 +13,6 @@ export async function GET({ locals, params }: RequestEvent) {
 
     return json({
         success: true,
-        order: items[params.orderID as any],
+        order: items.filter((order: order) => order.id === params.orderID)[0],
     });
 }
