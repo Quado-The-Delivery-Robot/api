eb590fb8c1848227f22141c2675db0a2956c39f1
Landon Redmond <landon.redmond0@gmail.com>
2024-03-01 10:33:15 -0600

    Add order get endpoint.

---
+++ src/routes/v1/orders/get/[orderID]/+server.ts
@@ -0,0 +1,18 @@
+import { json } from "@sveltejs/kit";
+import { getCollection } from "$lib/database";
+import type { Collection } from "mongodb";
+import type { RequestEvent } from "@sveltejs/kit";
+
+const ordersCollection: Collection = getCollection("core", "orders");
+
+export async function GET({ locals, params }: RequestEvent) {
+    const result = await ordersCollection.findOne({
+        id: locals.session.user?.email,
+        "item.id": params.orderID,
+    });
+
+    return json({
+        success: true,
+        order: result,
+    });
+}
